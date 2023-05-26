f3e9abd7213bdd67211af1e3a2b9ff62b675b48e
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-05-26 03:12:12 -0700

    docs: add license to headers ( PROOF-462 ) (#67)
    * docs: add license headers to code
    
    * docs: add license file to repo
    
    * chore: format code
    
    * docs: remove mode: rust comment from headers

--- src/sequences/dense_sequence.rs
+++ src/sequences/dense_sequence.rs
@@ -1,8 +1,16 @@
-// -*- mode: rust; -*-
+// Copyright 2023-present Space and Time Labs, Inc.
 //
-// Authors:
-// - Joe <joseribeiro1017@gmail.com>
-// - Ryan Burn <ryan@spaceandtime.io>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use byte_slice_cast::AsByteSlice;
 use curve25519_dalek::scalar::Scalar;
