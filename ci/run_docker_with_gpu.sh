60f862285eac4b9503a2b5876745ef82341b0bb6
Joe <joseribeiro1017@gmail.com>
2023-09-08 20:29:35 +0100

    ci: add docker execution scripts

---
+++ ci/run_docker_with_gpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# To build this image from source, see `sxt-utility-images` repo
+IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-0
+
+# If you have a GPU instance configured in your machine
+docker run -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE"
