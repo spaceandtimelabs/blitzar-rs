a8e77ae5d341080c0950d64db58fe4991f5ea506
José Ribeiro (Joe) <joseribeiro1017@gmail.com>
2023-09-18 18:11:22 +0100

    feat: setup semantic release and crates.io publishing ( PROOF-624 ) (#2)
    * feat: add package.json file
    
    * ci: add build script to repo
    
    * feat: add semantic-release workflow to repo
    
    * docs: add CONTRIBUTING.md file
    
    * chore: drop unnecessary folder from release
    
    * ci: fix typo
    
    * ci: add docs/ folder to zip

---
+++ ci/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eou pipefail
+
+NEW_VERSION=$1
+
+if ! [[ ${NEW_VERSION} =~ ^[0-9]+[.][0-9]+[.][0-9]+$ ]]
+then
+    echo "Incorrect version format: " $NEW_VERSION
+    exit 1
+fi
+
+# configure rust lib to release
+sed -i 's/version = "*.*.*" # This version will be automatically updated/version = "'${NEW_VERSION}'"/' Cargo.toml
+
+FILES="benches/ examples/ src/ Cargo.toml README.md docs/"
+
+cargo publish --allow-dirty --token ${CRATES_TOKEN}
+
+zip -r blitzar-v$NEW_VERSION.zip $FILES
+tar -czvf blitzar-v$NEW_VERSION.tar.gz $FILES
