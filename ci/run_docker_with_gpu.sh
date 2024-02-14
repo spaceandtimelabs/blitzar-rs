4af7f2197291bccac7d2b4867b3d54b13ae774c8
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-02-14 11:56:17 -0500

    update docker tag to pull latest version

--- ci/run_docker_with_gpu.sh
+++ ci/run_docker_with_gpu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE=spaceandtimelabs/blitzar:12.2.0-cuda-1.71.1-rust-0
+IMAGE=spaceandtimelabs/blitzar:12.3.1-cuda-1.76.0-rust-0
 
 # If you have a GPU instance configured in your machine
 docker run -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE"
