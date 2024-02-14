6d5ad39444fc36013d80fa032f57dd6fe65f68ab
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-02-14 12:37:32 -0500

    remove clippy from docker image

--- ci/docker/install_rust.sh
+++ ci/docker/install_rust.sh
@@ -15,6 +15,6 @@ source "$HOME/.cargo/env"
 # For latest version see https://github.com/rust-lang/rust/blob/master/RELEASES.md
 rustup install 1.76.0
 
-rustup component add rustfmt clippy
+rustup component add rustfmt
 
 $HOME/.cargo/bin/cargo install --version 0.69.4 bindgen-cli
