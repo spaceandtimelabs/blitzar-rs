2e53d1c7e4c839a60848f208f026d08c9e8f6a14
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2024-02-14 14:35:32 -0500

    update docker image

--- ci/run_docker_with_cpu.sh
+++ ci/run_docker_with_cpu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE=spaceandtimelabs/blitzar:12.3.1-cuda-1.76.0-rust-0
+IMAGE=rust:1.76
 
 # If you don't have a GPU instance configured in your machine
 docker run -v "$PWD":/src -w /src --privileged -it "$IMAGE"
--- ci/run_docker_with_gpu.sh
+++ ci/run_docker_with_gpu.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE=spaceandtimelabs/blitzar:12.3.1-cuda-1.76.0-rust-0
+IMAGE=rust:1.76
 
 # If you have a GPU instance configured in your machine
 docker run -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE"
