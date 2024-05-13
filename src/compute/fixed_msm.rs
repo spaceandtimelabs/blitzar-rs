ea531e8d0df04d8e634c0b0cfdef5715fca3e70d
rnburn <ryan.burn@gmail.com>
2024-05-13 12:15:43 -0700

    work on msm handle

--- src/compute/fixed_msm.rs
+++ src/compute/fixed_msm.rs
@@ -4,12 +4,14 @@ use std::marker::PhantomData;
 
 /// TODO(rnburn): document me
 pub struct MsmHandle<T: Curve> {
+    handle: *mut blitzar_sys::sxt_multiexp_handle,
     phantom: PhantomData<T>,
 }
 
 impl<T: Curve> MsmHandle<T> {
     fn new(generators: &[T] ) -> Self {
         Self{
+            handle: std::ptr::null_mut(),
             phantom: PhantomData,
         }
     }
