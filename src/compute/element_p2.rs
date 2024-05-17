066939222ed4527715a4d519f7e1bf91a77dd8f9
rnburn <ryan.burn@gmail.com>
2024-05-16 21:47:44 -0700

    work on wrapper

--- src/compute/element_p2.rs
+++ src/compute/element_p2.rs
@@ -1,6 +1,7 @@
-use std::convert::From;
-use ark_ec::short_weierstrass::{Affine, SWCurveConfig, Projective};
+use std::convert::{From, Into};
+use ark_ec::short_weierstrass::{Affine, SWCurveConfig};
 use ark_std::{One, Zero};
+use ark_ff::fields::Field;
 
 /// TODO(rnburn): doc me
 pub struct ElementP2<P: SWCurveConfig> {
@@ -14,13 +15,30 @@ pub struct ElementP2<P: SWCurveConfig> {
     pub z: P::BaseField,
 }
 
-impl<P: SWCurveConfig> From<Projective<P>> for ElementP2<P> {
-    fn from(pt: Projective<P>) -> Self {
-        let pt = Affine::<P>::from(pt);
+impl<P: SWCurveConfig> From<Affine<P>> for ElementP2<P> {
+    fn from(pt: Affine<P>) -> Self {
         Self{
             x: pt.x,
             y: pt.y,
             z: if pt.infinity { P::BaseField::zero() } else { P::BaseField::one() },
+        }
+    }
+}
+
+impl<P: SWCurveConfig> Into<Affine<P>> for ElementP2<P> {
+    fn into(self) -> Affine<P> {
+        if self.z.is_zero() {
+            return Affine::<P>{
+                x: P::BaseField::zero(),
+                y: P::BaseField::zero(),
+                infinity: true,
+            }
+        } 
+        let z_inv = self.z.inverse().unwrap();
+        Affine::<P>{
+            x: self.x * z_inv,
+            y: self.y * z_inv,
+            infinity: false,
         }
     }
 }
