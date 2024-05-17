c2c439dbbbe0fbe092735891130f475e9ee64c47
rnburn <ryan.burn@gmail.com>
2024-05-16 20:23:40 -0700

    fill in element_p2

--- src/compute/element_p2.rs
+++ src/compute/element_p2.rs
@@ -1,4 +1,26 @@
+use std::convert::From;
+use ark_ec::short_weierstrass::{Affine, SWCurveConfig, Projective};
+use ark_std::{One, Zero};
+
 /// TODO(rnburn): doc me
-pub struct ElementP2 {
+pub struct ElementP2<P: SWCurveConfig> {
+    /// TODO(rnburn): doc me
+    pub x: P::BaseField,
+
+    /// TODO(rnburn): doc me
+    pub y: P::BaseField,
+
+    /// TODO(rnburn): doc me
+    pub z: P::BaseField,
 }
 
+impl<P: SWCurveConfig> From<Projective<P>> for ElementP2<P> {
+    fn from(pt: Projective<P>) -> Self {
+        let pt = Affine::<P>::from(pt);
+        Self{
+            x: pt.x,
+            y: pt.y,
+            z: if pt.infinity { P::BaseField::zero() } else { P::BaseField::one() },
+        }
+    }
+}
