1298e10d4e28dcbcee0e3b8db0728c93f4901754
Ryan Burn <ryan.burn@gmail.com>
2024-08-19 12:02:00 -0700

    feat: add interface for variable length MSMs (PROOF-896) (#34)
    * bump blitzar
    
    * fill in vlen api
    
    * fill in tests
    
    * fill in vlen msm
    
    * add test

--- src/compute/fixed_msm.rs
+++ src/compute/fixed_msm.rs
@@ -112,6 +112,48 @@ impl<T: CurveId> MsmHandle<T> {
             );
         }
     }
+
+    /// Compute a varying lengthing multiexponentiation of scalars in packed format using a handle to
+    /// pre-specified generators.
+    ///
+    /// On completion `res` contains an array of size `num_outputs` for the multiexponentiation
+    /// of the given `scalars` array.
+    ///
+    /// An entry output_bit_table[output_index] specifies the number of scalar bits used for
+    /// output_index and output_lengths[output_index] specifies the length used for output_index.
+    ///
+    /// Note: output_lengths must be sorted in ascending order
+    ///
+    /// Put
+    ///     bit_sum = sum_{output_index} output_bit_table[output_index]
+    /// and let num_bytes denote the smallest integer greater than or equal to bit_sum that is a
+    /// multiple of 8.
+    ///
+    /// Let n denote the length of the longest output. Then `scalars` specifies a contiguous
+    /// multi-dimension `num_bytes` by `n` array laid out in a packed column-major order as specified by
+    /// output_bit_table. A given row determines the scalar exponents for generator g_i with the output
+    /// scalars packed contiguously and padded with zeros.
+    pub fn vlen_msm(
+        &self,
+        res: &mut [T],
+        output_bit_table: &[u32],
+        output_lengths: &[u32],
+        scalars: &[u8],
+    ) {
+        let num_outputs = res.len() as u32;
+        assert_eq!(output_bit_table.len(), num_outputs as usize);
+        assert_eq!(output_lengths.len(), num_outputs as usize);
+        unsafe {
+            blitzar_sys::sxt_fixed_vlen_multiexponentiation(
+                res.as_ptr() as *mut std::ffi::c_void,
+                self.handle,
+                output_bit_table.as_ptr(),
+                output_lengths.as_ptr(),
+                num_outputs,
+                scalars.as_ptr(),
+            );
+        }
+    }
 }
 
 impl<T: CurveId> Drop for MsmHandle<T> {
@@ -140,6 +182,15 @@ pub trait SwMsmHandle {
         output_bit_table: &[u32],
         scalars: &[u8],
     );
+
+    /// Compute a variable length MSM with the result given as affine elements
+    fn affine_vlen_msm(
+        &self,
+        res: &mut [Self::AffineElement],
+        output_bit_table: &[u32],
+        output_lengths: &[u32],
+        scalars: &[u8],
+    );
 }
 
 impl<C: SwCurveConfig + Clone> SwMsmHandle for MsmHandle<ElementP2<C>> {
@@ -166,6 +217,20 @@ impl<C: SwCurveConfig + Clone> SwMsmHandle for MsmHandle<ElementP2<C>> {
     ) {
         let mut res_p: Vec<ElementP2<C>> = vec![ElementP2::<C>::default(); res.len()];
         self.packed_msm(&mut res_p, output_bit_table, scalars);
+        res.par_iter_mut().zip(res_p).for_each(|(resi, resi_p)| {
+            *resi = resi_p.into();
+        });
+    }
+
+    fn affine_vlen_msm(
+        &self,
+        res: &mut [Self::AffineElement],
+        output_bit_table: &[u32],
+        output_lengths: &[u32],
+        scalars: &[u8],
+    ) {
+        let mut res_p: Vec<ElementP2<C>> = vec![ElementP2::<C>::default(); res.len()];
+        self.vlen_msm(&mut res_p, output_bit_table, output_lengths, scalars);
         res.par_iter_mut().zip(res_p).for_each(|(resi, resi_p)| {
             *resi = resi_p.into();
         });
--- src/compute/fixed_msm_tests.rs
+++ src/compute/fixed_msm_tests.rs
@@ -99,6 +99,29 @@ fn we_can_compute_packed_msms() {
 }
 
 #[test]
+fn we_can_compute_variable_length_msms() {
+    let mut rng = OsRng;
+
+    let mut res = vec![RistrettoPoint::default(); 2];
+
+    // randomly obtain the generator points
+    let generators: Vec<RistrettoPoint> =
+        (0..2).map(|_| RistrettoPoint::random(&mut rng)).collect();
+
+    // create handle
+    let handle = MsmHandle::new(&generators);
+
+    // g[0]
+    // g[0] + g[1]
+    let output_bit_table: Vec<u32> = vec![3, 1];
+    let output_lengths: Vec<u32> = vec![1, 2];
+    let scalars: Vec<u8> = vec![0b1001, 0b1011];
+    handle.vlen_msm(&mut res, &output_bit_table, &output_lengths, &scalars);
+    assert_eq!(res[0], generators[0]);
+    assert_eq!(res[1], generators[0] + generators[1]);
+}
+
+#[test]
 fn we_can_compute_msms_using_a_single_generator_bls12_381() {
     let mut rng = ark_std::test_rng();
 
@@ -142,5 +165,9 @@ fn for_short_weierstrass_curvs_we_can_compute_msms_with_affine_elements() {
 
     let output_bit_table: Vec<u32> = vec![2];
     handle.affine_packed_msm(&mut res, &output_bit_table, &scalars);
+    assert_eq!(res[0], g + g);
+
+    let output_lengths: Vec<u32> = vec![1];
+    handle.affine_vlen_msm(&mut res, &output_bit_table, &output_lengths, &scalars);
     assert_eq!(res[0], g + g);
 }
