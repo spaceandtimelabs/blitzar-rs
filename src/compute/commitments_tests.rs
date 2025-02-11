ff72cfa9d63103ecd5d0359355d54ac87e9abc36
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2025-02-11 15:19:35 -0500

    feat: add support for commitment computation with Halo2 BN256 points

--- src/compute/commitments_tests.rs
+++ src/compute/commitments_tests.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use super::*;
+use crate::compute::conversion::*;
 use ark_bls12_381::{Fr, G1Affine, G1Projective};
 use ark_bn254::{Fr as Bn254Fr, G1Affine as Bn254G1Affine, G1Projective as Bn254G1Projective};
 use ark_ec::{CurveGroup, VariableBaseMSM};
@@ -23,6 +24,7 @@ use curve25519_dalek::{
     ristretto::{CompressedRistretto, RistrettoPoint},
     scalar::Scalar,
 };
+use halo2curves::bn256::{G1Affine as Halo2Bn256G1Affine, G1 as Halo2Bn256G1Projective};
 use rand_core::OsRng;
 
 #[test]
@@ -543,6 +545,44 @@ fn sending_generators_to_gpu_produces_correct_bn254_g1_commitment_results() {
     // verify results
     assert_eq!(commitments[0], ark_commitment.into_affine());
     assert_ne!(Bn254G1Affine::default(), commitments[0]);
+}
+
+#[test]
+fn sending_halo2_generators_to_gpu_produces_correct_bn254_g1_commitment_results() {
+    // generate input table
+    let data: Vec<u64> = vec![2, 3, 1, 5, 4, 7, 6, 8, 9, 10];
+
+    // randomly obtain the generator points
+    let mut rng = rand::thread_rng();
+
+    let generator_points: Vec<Halo2Bn256G1Affine> = (0..data.len())
+        .map(|_| Halo2Bn256G1Affine::random(&mut rng))
+        .collect();
+
+    // initialize commitments
+    let mut commitments = vec![Halo2Bn256G1Projective::default(); 1];
+
+    // compute commitment in Blitzar
+    compute_bn254_g1_uncompressed_commitments_with_halo2_generators(
+        &mut commitments,
+        &[(&data).into()],
+        &generator_points,
+    );
+
+    // convert data to scalar
+    let scalar_data: Vec<Bn254Fr> = data.iter().map(|&d| Bn254Fr::from(d)).collect();
+
+    // convert generators to Arkworks
+    let ark_generator_points: Vec<Bn254G1Affine> =
+        convert_bn254_g1_affine_generators_from_halo2_to_ark(&generator_points);
+
+    // compute msm in Arkworks
+    let ark_commitment = Bn254G1Projective::msm(&ark_generator_points, &scalar_data).unwrap();
+
+    // verify results
+    let result_commitments = convert_commitments_from_halo2_to_arkworks(&commitments);
+    assert_eq!(result_commitments[0], ark_commitment.into_affine());
+    assert_ne!(Bn254G1Affine::default(), result_commitments[0]);
 }
 
 #[test]
