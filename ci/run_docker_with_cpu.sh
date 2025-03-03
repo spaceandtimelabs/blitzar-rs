9de37a2b14e6671ae9cd5a52ecaa59042b69da75
Jacob Trombetta <jacob.trombetta@spaceandtime.io>
2025-03-03 12:03:50 -0500

    refactor: remove Docker files from ci folder (#66)
    refactor: remove Docker files from CI folder

--- ci/run_docker_with_cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IMAGE=rust:1.84
-
-# If you don't have a GPU instance configured in your machine
-docker run -v "$PWD":/src -w /src --privileged -it "$IMAGE"
--- ci/run_docker_with_gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IMAGE=rust:1.84
-
-# If you have a GPU instance configured in your machine
-docker run -v "$PWD":/src -w /src --gpus all --privileged -it "$IMAGE"
