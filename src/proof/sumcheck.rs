f392008511faa484009bd4f7912e9392fadfedc0
rnburn <ryan.burn@gmail.com>
2025-03-04 18:14:54 -0800

    fill in sumcheck

--- src/proof/sumcheck.rs
+++ src/proof/sumcheck.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
 use crate::proof::field::FieldId;
 use crate::proof::sumcheck_transcript::SumcheckTranscript;
+use serde::{Deserialize, Serialize};
 use std::os::raw::c_void;
 
 /// SumcheckProof construct
@@ -14,10 +14,10 @@ impl<T: FieldId + Default + Clone> SumcheckProof<T> {
     /// TODO: doc me
     pub fn new<Transcript: SumcheckTranscript<T>>(
         transcript: &mut Transcript,
-        mles: & [T],
+        mles: &[T],
         product_table: &[(T, u32)],
         product_terms: &[u32],
-        n: u32
+        n: u32,
     ) -> Self {
         let num_mles = mles.len() / n as usize;
         assert_eq!(mles.len(), num_mles * n as usize);
@@ -29,9 +29,9 @@ impl<T: FieldId + Default + Clone> SumcheckProof<T> {
 
         transcript.init(num_rounds, round_degree);
 
-        let fptr : fn(*mut T, *mut c_void, *const T, u32) = round_challenge::<T, Transcript>;
+        let fptr: fn(*mut T, *mut c_void, *const T, u32) = round_challenge::<T, Transcript>;
 
-        let mut descriptor: blitzar_sys::sumcheck_descriptor = blitzar_sys::sumcheck_descriptor{
+        let mut descriptor: blitzar_sys::sumcheck_descriptor = blitzar_sys::sumcheck_descriptor {
             mles: mles.as_ptr() as *const c_void,
             product_table: product_table.as_ptr() as *const c_void,
             product_terms: product_terms.as_ptr(),
@@ -41,20 +41,18 @@ impl<T: FieldId + Default + Clone> SumcheckProof<T> {
             num_product_terms: product_terms.len() as u32,
             round_degree: round_degree as u32,
         };
-        // descriptor.mles = mles.as_ptr() as *c_void;
-        // unsafe {
-        //     blitzar_sys::
-        // }
-            // let handle = blitzar_sys::sxt_multiexp_handle_new(
-            //     T::CURVE_ID,
-            //     generators.as_ptr() as *const std::ffi::c_void,
-            //     generators.len() as u32,
-            // );
-        // fn f(ctx: *mut c_void, polynomial: *const T) {
-        // }
-        // void (*)(T* r, void* context, const T* polynomial, unsigned polynomial_len);
-
-        Self{
+        unsafe {
+            blitzar_sys::sxt_prove_sumcheck(
+                round_polynomials.as_ptr() as *mut c_void,
+                evaluation_point.as_ptr() as *mut c_void,
+                T::FIELD_ID,
+                &descriptor,
+                fptr as *mut c_void,
+                // transcript_ptr as *mut c_void,
+                std::ptr::from_ref(transcript) as *mut c_void,
+            );
+        }
+        Self {
             evaluation_point: evaluation_point,
             round_polynomials: round_polynomials,
         }
@@ -63,10 +61,13 @@ impl<T: FieldId + Default + Clone> SumcheckProof<T> {
 
 fn round_challenge<T, Transcript: SumcheckTranscript<T>>(
     r: *mut T,
-    ctx: *mut c_void, polynomial: *const T, len: u32) {
+    ctx: *mut c_void,
+    polynomial: *const T,
+    len: u32,
+) {
     unsafe {
-    let mut transcript = &mut *(ctx as *mut Transcript);
-    let p = std::slice::from_raw_parts(polynomial, len as usize);
-    *r.as_mut().unwrap() = transcript.round_challenge(p);
+        let transcript = &mut *(ctx as *mut Transcript);
+        let p = std::slice::from_raw_parts(polynomial, len as usize);
+        *r.as_mut().unwrap() = transcript.round_challenge(p);
     }
 }
