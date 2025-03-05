c61ba64787aedb37a2d361c5d31ff509c8963509
rnburn <ryan.burn@gmail.com>
2025-03-04 18:01:52 -0800

    fill in sumcheck

--- src/proof/sumcheck.rs
+++ src/proof/sumcheck.rs
@@ -19,6 +19,8 @@ impl<T: FieldId + Default + Clone> SumcheckProof<T> {
         product_terms: &[u32],
         n: u32
     ) -> Self {
+        let num_mles = mles.len() / n as usize;
+        assert_eq!(mles.len(), num_mles * n as usize);
         let num_rounds = n.next_power_of_two().trailing_zeros() as usize;
         let mut evaluation_point = vec![T::default(); num_rounds];
         let round_degree = product_table.iter().map(|entry| entry.1).max().unwrap() as usize;
@@ -28,6 +30,26 @@ impl<T: FieldId + Default + Clone> SumcheckProof<T> {
         transcript.init(num_rounds, round_degree);
 
         let fptr : fn(*mut T, *mut c_void, *const T, u32) = round_challenge::<T, Transcript>;
+
+        let mut descriptor: blitzar_sys::sumcheck_descriptor = blitzar_sys::sumcheck_descriptor{
+            mles: mles.as_ptr() as *const c_void,
+            product_table: product_table.as_ptr() as *const c_void,
+            product_terms: product_terms.as_ptr(),
+            n: n,
+            num_mles: num_mles as u32,
+            num_products: product_table.len() as u32,
+            num_product_terms: product_terms.len() as u32,
+            round_degree: round_degree as u32,
+        };
+        // descriptor.mles = mles.as_ptr() as *c_void;
+        // unsafe {
+        //     blitzar_sys::
+        // }
+            // let handle = blitzar_sys::sxt_multiexp_handle_new(
+            //     T::CURVE_ID,
+            //     generators.as_ptr() as *const std::ffi::c_void,
+            //     generators.len() as u32,
+            // );
         // fn f(ctx: *mut c_void, polynomial: *const T) {
         // }
         // void (*)(T* r, void* context, const T* polynomial, unsigned polynomial_len);
