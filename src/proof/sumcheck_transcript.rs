9068c8bf9dbb1af4a6cb4790a0a803704ff2195c
rnburn <ryan.burn@gmail.com>
2025-03-10 21:41:34 -0700

    doc

--- src/proof/sumcheck_transcript.rs
+++ src/proof/sumcheck_transcript.rs
@@ -1,8 +1,10 @@
-/// todo: doc me
+/// Provide random challenges for sumcheck proof.
 pub trait SumcheckTranscript<T> {
-    /// todo: doc me
+    /// Provides dimensions of sumcheck proof.
     fn init(&mut self, num_variables: usize, round_degree: usize);
 
-    /// todo: doc me
+    /// Produce the challenge field element for a sumcheck
+    /// round given the round polynomial
+    ///   polynomial[0] + polynomial[1] X^1 + ... + polynomial[d] X^d
     fn round_challenge(&mut self, polynomial: &[T]) -> T;
 }
